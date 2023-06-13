0d764ccd2795c3a35c4f61057bda435cead9f60c
yasminoliveirat <yasminoliveira_santos@hotmail.com>
2023-06-13 01:00:33 -0300

    tentando fazer cadastro

--- Cadastro/code.js
+++ Cadastro/code.js
@@ -1,39 +1,17 @@
-function cadastrar(event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-
-    // Obtenha os valores dos campos de entrada
-    var nome = document.querySelector('input[name="Nome"]').value;
-    var celular = document.querySelector('input[name="Celular"]').value;
-    var email = document.querySelector('input[name="Email"]').value;
-    var senha = document.querySelector('input[name="Senha"]').value;
-    var confirmarSenha = document.querySelector('input[name="ConfirmarSenha"]').value;
-
-    // Faça a validação dos campos (opcional)
-
-    // Exemplo de validação básica
-    if (nome === '' || celular === '' || email === '' || senha === '' || confirmarSenha === '') {
-        alert('Por favor, preencha todos os campos!');
-        return;
+function cadastro() {
+        var dados = JSON.parse(localStorage.getItem("tds"))
+        localStorage.removeItem("tds")
+        let id = document.querySelector("#id").value
+        let login = document.querySelector("#login").value
+        let senha = document.querySelector("#senha").value
+    
+        for (let i = 0; dados.length > i; i++) {
+        if (id == dados[i].id){
+            let user = { id: id, nome: login, senha: senha }
+            dados[i] = user
+            localStorage.setItem("tds", JSON.stringify(dados))
+            alert("Atualizado!")
+            break
+            } 
+        }
     }
-
-    // Verifique se as senhas correspondem
-    if (senha !== confirmarSenha) {
-        alert('As senhas não correspondem!');
-        return;
-    }
-
-    // Envie os dados do formulário para onde quiser (por exemplo, um servidor)
-
-    // Exemplo de exibição dos dados enviados
-    console.log('Nome:', nome);
-    console.log('Celular:', celular);
-    console.log('Email:', email);
-    console.log('Senha:', senha);
-    console.log('Confirmar Senha:', confirmarSenha);
-
-    // Limpe os campos do formulário após o envio
-    document.querySelector('form').reset();
-}
-
-// Adicione um ouvinte de evento ao formulário para capturar o envio
-document.querySelector('form').addEventListener('submit', cadastrar);
