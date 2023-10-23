123469d5d10a9e1891f99bd69c7fae410c325037
yasminoliveirat <yasminoliveira_santos@hotmail.com>
2023-10-22 22:10:06 -0300

    Estilizando Aba Pagamento

---
+++ Produtos/scriptcarrinho.js
@@ -0,0 +1,10 @@
+const direita = document.getElementById('direita');
+const adicionarFormaPagamentoButton = document.getElementById('adicionar-forma-pagamento');
+
+direita.addEventListener('mouseenter', () => {
+    adicionarFormaPagamentoButton.style.display = 'block'; // Exibe o botão ao passar o mouse sobre a div
+});
+
+direita.addEventListener('mouseleave', () => {
+    adicionarFormaPagamentoButton.style.display = 'none'; // Oculta o botão ao tirar o mouse da div
+});
