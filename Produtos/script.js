f176002337e384cc4a0908b24fb8f1f6ce97ee29
Yasmin <yasminoliveira_santos@hotmail.com>
2023-11-06 09:21:58 -0300

    Back end resumo do pedido

--- Produtos/script.js
+++ Produtos/script.js
@@ -24,6 +24,35 @@ function adicionarProduto(nome, preco) {
  
   exibirCarrinho();
 
+// resumo pedido
+function adicionarProduto(nome, preco) {
+  var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
+  carrinho.push({ nome: nome, preco: preco });
+  sessionStorage.setItem("carrinho", JSON.stringify(carrinho));
+  exibirCarrinho();
+}
+
+function exibirCarrinho() {
+  var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
+  var listaItens = document.getElementById("itensresumo");
+  var total = 0;
+
+  listaItens.innerHTML = "";
+  carrinho.forEach(function(item) {
+    var li = document.createElement("li");
+    li.textContent = item.nome + " - R$" + item.preco.toFixed(2);
+    listaItens.appendChild(li);
+    total += item.preco;
+  });
+
+  document.getElementById("totall").textContent = total.toFixed(2);
+}
+
+
+exibirCarrinho();
+
+//contador
+
   var contador = 0;
 
 function incrementar() {
