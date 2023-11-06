f30b59785a9eb1365f7d45d0267c66b6544d316c
Dandara <dands20melo@gmail.com>
2023-11-06 20:57:40 -0300

    Adição de arquivo js, alterações no front da página pagamento e carrinho

---
+++ Pagamento/scriptpag.js
@@ -0,0 +1,23 @@
+// Parte do front
+
+const showContentRadio = document.getElementById('show-content-radio');
+const content = document.getElementById('content');
+
+showContentRadio.addEventListener('change', function() {
+    if (showContentRadio.checked) {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+});
+
+const showContentRadio3 = document.getElementById('show-content-radio3');
+const content3 = document.getElementById('content3');
+
+showContentRadio3.addEventListener('change', function() {
+    if (showContentRadio3.checked) {
+      content3.style.display = 'block';
+    } else {
+      content3.style.display = 'none';
+    }
+});
