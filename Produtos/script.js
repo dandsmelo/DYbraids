ca10e226ecc3c1f31cc92ba3b3f4640a5c49fa8f
Yasmin <yasminoliveira_santos@hotmail.com>
2023-11-06 10:35:07 -0300

    Back end Carrinho

--- Produtos/script.js
+++ Produtos/script.js
@@ -1,30 +1,4 @@
-function adicionarProduto(nome, preco) {
-    var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
-    carrinho.push({ nome: nome, preco: preco });
-    sessionStorage.setItem("carrinho", JSON.stringify(carrinho));
-    exibirCarrinho();
-  }
-
-  function exibirCarrinho() {
-    var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
-    var listaItens = document.getElementById("itensCarrinho");
-    var total = 0;
-
-    listaItens.innerHTML = "";
-    carrinho.forEach(function(item) {
-      var li = document.createElement("li");
-      li.textContent = item.nome + " - R$" + item.preco.toFixed(2);
-      listaItens.appendChild(li);
-      total += item.preco;
-    });
-
-    document.getElementById("total").textContent = total.toFixed(2);
-  }
-
- 
-  exibirCarrinho();
-
-// resumo pedido
+// Carrinho
 function adicionarProduto(nome, preco) {
   var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
   carrinho.push({ nome: nome, preco: preco });
@@ -33,6 +7,32 @@ function adicionarProduto(nome, preco) {
 }
 
 function exibirCarrinho() {
+  var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
+  var listaItens = document.getElementById("itensCarrinho");
+  var total = 0;
+
+  listaItens.innerHTML = "";
+  carrinho.forEach(function(item) {
+    var li = document.createElement("li");
+    li.textContent = item.nome + " - R$" + item.preco.toFixed(2);
+    listaItens.appendChild(li);
+    total += item.preco;
+  });
+
+  document.getElementById("total").textContent = total.toFixed(2);
+}
+
+exibirCarrinho();
+
+// Resumo do pedido 
+function adicionarProdutoResumo(nome, preco) {
+  var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
+  carrinho.push({ nome: nome, preco: preco });
+  sessionStorage.setItem("carrinho", JSON.stringify(carrinho));
+  exibirCarrinhoResumo();
+}
+
+function exibirCarrinhoResumo() {
   var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
   var listaItens = document.getElementById("itensresumo");
   var total = 0;
@@ -48,8 +48,8 @@ function exibirCarrinho() {
   document.getElementById("totall").textContent = total.toFixed(2);
 }
 
+exibirCarrinhoResumo();
 
-exibirCarrinho();
 
 //contador
 
