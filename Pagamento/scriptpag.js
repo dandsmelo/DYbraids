91bdd265b6cd6d0018e1bb311e7c5e99eddcff9f
Dandara <dands20melo@gmail.com>
2023-11-08 18:30:30 -0300

    Ajustando resumo do pagamento

--- Pagamento/scriptpag.js
+++ Pagamento/scriptpag.js
@@ -22,6 +22,26 @@ showContentRadio3.addEventListener('change', function() {
     }
 });
 
+
+function exibirCarrinhoResumo() {
+  var carrinho = JSON.parse(sessionStorage.getItem("carrinho")) || [];
+  var listaItens = document.getElementById("itensresumo");
+  var total = 0;
+
+  listaItens.innerHTML = "";
+
+  carrinho.forEach(function (item) {
+    var li = document.createElement("li");
+    li.textContent = item.nome + " --- R$" + (item.preco * item.quantidade).toFixed(2);
+    listaItens.appendChild(li);
+    total += item.preco * item.quantidade;
+  });
+
+  document.getElementById("totall").textContent = total.toFixed(2);
+}
+
+exibirCarrinhoResumo();
+
 //função de redirecionamento
 function redirecionarFim(){
   var URL = "/Pagamento/compraRealizada.html";
